97fd2a603449964f3e5e921fe3f8ac0b48703135
ProfessroDanilo <djkcond@gmail.com>
2024-04-13 21:02:32 -0300

    feita a parte com flask para ler serial e enviar para html

--- flask/serial_to_web.py
+++ flask/serial_to_web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 # Configuração da porta serial
-port = 'COM9'  # Substitua pelo nome da porta COM do Arduino
+port = '/dev/ttyUSB0'  # Substitua pelo nome da porta COM do Arduino
 baudrate = 9600
 
 # Abre a porta serial
@@ -14,8 +14,8 @@ ser = serial.Serial(port, baudrate)
 @app.route('/')
 def index():
     # Lê os dados da porta serial
-    #data = ser.readline().decode().strip()
-    return render_template('index.html', data="teste")
+    data = ser.readline().decode().strip()
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
