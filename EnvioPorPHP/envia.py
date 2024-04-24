ef595603360a5e5e280d03430493329cbe463485
ProfessorDanilo <ProfessorDanilo@github.com>
2024-04-23 22:34:11 -0300

    atualizando envia.py

--- EnvioPorPHP/envia.py
+++ EnvioPorPHP/envia.py
@@ -7,16 +7,18 @@ port = '/dev/ttyUSB0'
 #velocidade de comunicação serial
 baudrate = 9600
 
-# Abre a porta serial
-ser = serial.Serial(port, baudrate)
-
 # URL do formulário PHP
 url = 'https://profdanilo.com.br/tcc/receber_nivel.php'  
 contador = 0
 try:
     while True:
+        # Abre a porta serial
+        ser = serial.Serial(port, baudrate)
+
         # Ler dados da porta serial
         data = ser.readline().decode().strip()
+        time.sleep(1)
+        ser.close()
 
         # Enviar dados para o servidor via requisição POST
         payload = {'nivel': data}  # Dados a serem enviados para o servidor
@@ -28,8 +30,6 @@ try:
             print('Dados enviados com sucesso (',contador,'):',  data)
         else:
             print('Erro ao enviar dados:', response.text)
-        time.sleep(600)
 
-except KeyboardInterrupt:
-    ser.close()
+except KeyboardInterrupt:    
     print("Falha ao enviar o arquivo")
